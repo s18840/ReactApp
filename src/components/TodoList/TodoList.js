a3af70b3e23d9ece08a7b8fe8ebe88ce640227c6
msroczynsk003 <mikolaj.sroczynski@pwc.com>
2024-02-06 13:55:41 +0100

    added checkboxes and styling

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,14 +1,18 @@
-import '../../styles/TodoList/TodoList.css'
-import TodoItem from './TodoItem';
+import "../../styles/TodoList/TodoList.css";
+import TodoItem from "./TodoItem";
+import { FaPlusCircle } from "react-icons/fa";
 
 function ToDolist() {
-
-    
   return (
     <div className="main-container">
-      <TodoItem iconName={"FaClock"} itemName={'Wake up'}/>
-      <TodoItem iconName={"FaHamburger"} itemName={'Eat breakfast'}/>
-      <TodoItem iconName={"FaShower"} itemName={'Take a shower'}/>
+      <TodoItem iconName={"FaClock"} itemName={"Wake up"} />
+      <TodoItem iconName={"FaHamburger"} itemName={"Eat breakfast"} />
+      <TodoItem iconName={"FaShower"} itemName={"Take a shower"} />
+      <div className="add-new-item-container">
+        <div className="add-new-icon"></div>
+        <input type="text" placeholder="Enter a todo..." />
+        <div className="item-round-button" onClick={console.log("clicked")}><FaPlusCircle className="item-icon-white" /></div>
+      </div>
     </div>
   );
 }
